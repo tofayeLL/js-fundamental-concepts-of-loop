17f78a7fd9beb72d8020dc0589e230a3d4fabbcf
tofayeLL <ahmedtofayel132@gmail.com>
2024-01-29 22:01:05 +0600

    learn and practice for--of

---
+++ for-of.js
@@ -0,0 +1,19 @@
+
+/* const numbers = [12, 1, 23, 5, 67, 8, 9];
+console.log(numbers);
+for(const number of numbers){
+    console.log(number);
+}
+ */
+
+
+/* -----structure for loop */
+/* -----for(condition){} */
+
+
+const fruits = ['apple', 'mango', 'banana', 'lichi'];
+console.log(fruits);
+for(const fruit of fruits){
+    console.log(fruit);
+    // console.log('i want to it');
+}
---
+++ revision.js
@@ -0,0 +1,25 @@
+
+// about ---variable---
+/*
+* const: value will not change .
+*let: value can change.
+*var: we should not use it.
+*/
+
+
+// variable
+const amount = 200;
+
+
+// condition
+
+if(amount > 150){
+
+}
+else{
+
+}
+
+
+// array
+const numbers = [10, 11, 2, 4, 6, 9, 20];
