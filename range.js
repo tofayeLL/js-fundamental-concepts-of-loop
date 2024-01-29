d6fd70182e58d6b91a17b637419790bd2701132a
tofayeLL <ahmedtofayel132@gmail.com>
2024-01-29 22:50:22 +0600

    learn and practice about while loop

---
+++ range.js
@@ -0,0 +1,27 @@
+
+/* show output 1 to 10 by using while loop */
+/* let num = 1;
+while(num <= 10){
+    console.log(num);
+    num++;
+} */
+
+
+/* show output 50 to 100 by using while loop */
+let num = 50;
+while(num <= 100){
+    console.log(num);
+    num++;
+}
+
+
+
+
+
+
+
+
+
+
+
+
