6e12bed76804dd7a46aee6454a58b5e4f8086fee
tofayeLL <ahmedtofayel132@gmail.com>
2024-01-30 12:41:15 +0600

    finished practice task three

--- practice-task-for.js
+++ practice-task-for.js
@@ -10,16 +10,45 @@
 /* ----practice task two---- */
 /* ---odd numbers----- */
 
-for(let i = 61; i <= 100; i++){
+/* for(let i = 61; i <= 100; i++){
   if(i % 2 === 1){
     console.log(i);
   }
 }
-
+ */
 
 /* -----even numbers---- */
-for(let i = 78; i <= 98; i++){
+/* for(let i = 78; i <= 98; i++){
    if(i % 2 === 0){
     console.log(i);
    }
+} */
+
+
+
+/* ------practice task three----- */
+/* ---sum of odd numbers--- */
+/* let total = 0;
+for(let i = 91; i <= 129; i++){
+    if(i % 2 === 1){
+        // console.log(i);
+        total = total + i;
+        // console.log(total);
+    }
 }
+console.log('Total sum of odd numbers:', total); */
+
+
+/* -------sum of even numbers----- */
+let sum = 0;
+for(let i = 51; i <= 85; i++){
+    if(i  % 2 === 0){
+        // console.log(i);
+        sum = sum + i;
+        // console.log(sum);
+    }
+}
+console.log('Total sum of even numbers:', sum);
+
+
+
