58d9189a2b54ec2459a7f29f58ccb7d790ae5485
tofayeLL <ahmedtofayel132@gmail.com>
2024-01-30 14:01:26 +0600

    finished practice task one by using break

---
+++ practice-break.js
@@ -0,0 +1,20 @@
+
+
+/* ---practice task one----- */
+for(let i = 1; i <= 200; i++){
+    if(i === 100){
+        break;
+    }
+    console.log(i);
+}
+
+
+// by using while
+let n = 1;
+while(n <= 200){
+   if(n === 100){
+    break;
+   }
+   console.log(n);
+   n++;
+}
