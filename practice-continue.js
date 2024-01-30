f1d781c09945f9245b5f530f044dffb13e24790a
tofayeLL <ahmedtofayel132@gmail.com>
2024-01-30 13:54:42 +0600

    finished practice task two by use continue

--- practice-continue.js
+++ practice-continue.js
@@ -3,16 +3,16 @@
 /* ------practice task 1---- */
 /* --show even numbers and skip odd numbers-- */
 // by using for loop
-for(let i = 1; i <= 40; i++){
+/* for(let i = 1; i <= 40; i++){
     if(i % 2 !== 0){
        continue;
     }
   console.log(i);
-}
+} */
 
 
 // by using while loop
-let n = 1;
+/* let n = 1;
 while(n <= 40){
     n++;
     if((n % 2) === 1){
@@ -20,4 +20,37 @@ while(n <= 40){
     }
     console.log(n);
    
+} */
+
+
+
+
+
+
+
+/* ------practice task 2------- */
+/* -----odd numbers but----- */
+// by using for loop
+for(let i = 55; i <= 85; i++){
+    if(i % 2 === 1){
+        if(i % 5 === 0){
+            continue;
+        }
+        console.log(i);
+    }
+}
+
+
+// by using while loop
+let n = 55;
+while(n <= 85){
+    n++;
+    if(n % 2 !== 0){
+       
+        if(n % 5 === 0){
+            continue;
+        }
+        console.log(n);
+    }
+    
 }
