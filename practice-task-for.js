41963e20e7b4665c11ec940e73dcfbab8809036e
tofayeLL <ahmedtofayel132@gmail.com>
2024-01-30 12:31:02 +0600

    finished practice task one and two

---
+++ practice-task-for.js
@@ -0,0 +1,25 @@
+
+
+/* ----practice task one---- */
+/* for(let i = 1; i <= 60; i++){
+    console.log('I will invest at least 6 hours every single day for the next 6 months');
+} */
+
+
+
+/* ----practice task two---- */
+/* ---odd numbers----- */
+
+for(let i = 61; i <= 100; i++){
+  if(i % 2 === 1){
+    console.log(i);
+  }
+}
+
+
+/* -----even numbers---- */
+for(let i = 78; i <= 98; i++){
+   if(i % 2 === 0){
+    console.log(i);
+   }
+}
