50ec1cd93c09c84d859dab270f1b2572f73c8174
tofayeLL <ahmedtofayel132@gmail.com>
2024-01-30 11:24:03 +0600

    finished practice task one about while loop

---
+++ practice-while.js
@@ -0,0 +1,10 @@
+
+
+// practice --1
+
+let count = 1;
+while (count <= 60) {
+    // console.log(count);
+    console.log('I will invest at least 6 hours every single day for the next 6 months')
+    count++;
+}
