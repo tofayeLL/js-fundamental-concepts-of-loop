447c6c2f6c3e3efc3c69a088d5bea4773811d7d1
tofayeLL <ahmedtofayel132@gmail.com>
2024-01-30 11:29:54 +0600

    finished practice task two about while loop odd numbers

--- practice-while.js
+++ practice-while.js
@@ -2,9 +2,20 @@
 
 // practice --1
 
-let count = 1;
+/* let count = 1;
 while (count <= 60) {
     // console.log(count);
     console.log('I will invest at least 6 hours every single day for the next 6 months')
     count++;
+} */
+
+
+// ---practice task ---2--
+// -----odd numbers----
+let n = 61;
+while(n <= 100){
+    if(n % 2 === 1){
+        console.log(n);
+    }
+    n++;
 }
